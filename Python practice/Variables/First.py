c9d40fd5d6cc2ddb32c7c81dca6700c1ba167436
Rohit Sahay <rohitsahay399-cell>
2026-07-11 16:38:47 +0530

    git add . git commit -m "Added Python variables practice"

---
+++ Python practice/Variables/First.py
@@ -0,0 +1,16 @@
+#INTEGER
+age = 5
+
+#FLOAT
+height = 5.9
+
+#STRING
+name = "Rohit Sahay"
+
+#BOOLEAN
+is_adult = False
+
+print("Age:",age)
+print("Height:",height)
+print("Name:",name)
+print("Is Adult:",is_adult)
