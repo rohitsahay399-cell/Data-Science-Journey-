baf3adc0baa07686cae944a4d59dec6c4f872612
Rohit Sahay <rohit.sahay399@gmail.com>
2026-07-22 14:05:54 +0530

    Add Arithmetic operations script for basic calculations

---
+++ Python practice/Arithmetic_operation.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("Sum:", num1 + num2)
+print("Differnce:", num1 - num2)
+print("Product:", num1 * num2)
+if num2 !=0:
+    print("Quotient", num1 / num2)
+else:
+    print(("Division: cannot divide by zero"))
+    
