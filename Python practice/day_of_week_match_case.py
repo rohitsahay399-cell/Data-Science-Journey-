8f779d944cfb2455f1eccbaa47492f45287d4bf8
Rohit Sahay <rohit.sahay399@gmail.com>
2026-07-24 00:12:59 +0530

    Add Day of the Week script using match-case to display the corresponding day

---
+++ Python practice/day_of_week_match_case.py
@@ -0,0 +1,29 @@
+day = int(input("Enter the day number (1-7): "))
+
+match day:
+    case 1:
+        print("Monday")
+
+    case 2:
+     print("Tuesday")
+
+    case 3:
+        print("Wednesday")
+
+    case 4:
+        print("Thursday")
+
+    case 5:
+     print("Friday")
+
+    case 6:
+     print("Saturday")
+
+    case 7:
+     print("Sunday")
+
+    case _:
+     print("Invalid day number. Please enter a number between 1 and 7.")
+
+
+        
