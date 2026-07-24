25ee94beaf138edc87685bebaaa3b2cd074e9ed7
Rohit Sahay <rohit.sahay399@gmail.com>
2026-07-25 01:09:31 +0530

    Add password checker script with input validation

---
+++ Python practice/password_checker.py
@@ -0,0 +1,8 @@
+Correct_Password = "Rohit@123"
+Password = input("Enter Your Password:")
+
+while Password != Correct_Password:
+    print("Incorrect  password, Please try again.")
+
+    Password = input("Enter Your Passwword:")
+    print("Access Granted")
