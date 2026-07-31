1c41cb401bcce7e9d88f6f70bc37149cfccf6b19
Rohit Sahay <rohit.sahay399@gmail.com>
2026-08-01 00:52:28 +0530

    Add CheckIsAlnum.py to verify if a string contains only alphabets and digits

---
+++ Python practice/Strings/CheckIsAlnum.py
@@ -0,0 +1,6 @@
+text = input("Enter a string: ")
+
+if text.isalnum():
+    print("String contains only alphabets and digits.")
+else:
+    print("String does not contain only alphabets and digits.")
