75453e2bab62f792c6a119184b91804d05e9df52
Rohit Sahay <rohit.sahay399@gmail.com>
2026-08-08 00:49:48 +0530

    Add Area_perimeter.py to calculate the area and perimeter of a rectangle

---
+++ Python practice/Variables/Area_parimeter.py
@@ -0,0 +1,9 @@
+num1= input("Emter the number: ")
+num2 = input("Enter the second number: ")
+area = float(num1) * float(num2)
+perimeter = 2 * (float(num1) + float (num2))
+print("Area of rectangle is", area)
+print("perimeter of rectangle is" , perimeter)
+ 
+                                            
+                                                          
